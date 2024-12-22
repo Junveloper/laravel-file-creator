e2e4ed1f4fa05a7ca7648838b37f238a7df6499d
Jun <55792006+Junveloper@users.noreply.github.com>
2024-12-22 19:36:30 +1000

    add menu

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,45 @@ import Creator, { LaravelFileTypes } from "./Creator";
 export function activate(context: vscode.ExtensionContext) {
   const creator = new Creator();
 
+  const createLaravelFile = vscode.commands.registerCommand(
+    "laravelFileCreator.createLaravelFile",
+    async (folder) => {
+      if (!folder) {
+        folder = await vscode.window.showOpenDialog({
+          canSelectFiles: false,
+          canSelectFolders: true,
+          canSelectMany: false,
+        });
+
+        if (!folder || !folder[0]) {
+          return;
+        }
+
+        folder = folder[0];
+      }
+
+      // Display a Quick Pick menu to select file type
+      const fileType = await vscode.window.showQuickPick(
+        [
+          {
+            label: "Single Action Controller",
+            value: LaravelFileTypes.SingleActionController,
+          },
+          { label: "Form Request", value: LaravelFileTypes.FormRequest },
+          { label: "Model", value: LaravelFileTypes.Model },
+        ],
+        { placeHolder: "Select the type of file to create" }
+      );
+
+      if (!fileType) {
+        return; // Exit if the user cancels the Quick Pick
+      }
+
+      // Call the createFile method with the selected type
+      creator.createFile(fileType.value, folder);
+    }
+  );
+
   const createSingleActionController = vscode.commands.registerCommand(
     "laravelFileCreator.createSingleActionController",
     (folder) =>
@@ -20,6 +59,7 @@ export function activate(context: vscode.ExtensionContext) {
     (folder) => creator.createFile(LaravelFileTypes.Model, folder)
   );
 
+  context.subscriptions.push(createLaravelFile);
   context.subscriptions.push(createSingleActionController);
   context.subscriptions.push(createFormRequest);
   context.subscriptions.push(createModel);
